5ee4489741fc9a0d0fb3d0cb72c0154ad2afa5d6
IngDavidTM <david-1011@hotmail.com>
2022-12-13 15:19:06 -0500

    add constraint

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -9,7 +9,7 @@ CREATE TABLE medical_histories(
   id INT GENERATED ALWAYS AS IDENTITY,
   admitted_at timestamp,
   patient_id INT,
-  FOREIGN KEY (patient_id) REFERENCES patients(id),
+  CONSTRAINT patient_id_fk  FOREIGN KEY (patient_id) REFERENCES patients(id),
   status VARCHAR(250),
   PRIMARY KEY(id)
 );
@@ -20,7 +20,7 @@ CREATE TABLE invoices(
   generated_at timestamp,
   payed_at timestamp,
   medical_history_id INT,
-  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+  CONSTRAINT medical_history_id_fk FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
   PRIMARY KEY(id)
 );
 
@@ -38,8 +38,8 @@ quantity INT,
 total_price decimal,
 invoice_id INT,
 treatment_id INT,
-FOREIGN KEY (invoice_id) REFERENCES invoices(id),
-FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+CONSTRAINT invoice_id_fk FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+CONSTRAINT treatment_id_fk FOREIGN KEY (treatment_id) REFERENCES treatments(id),
 PRIMARY KEY(id)
 );
 
@@ -47,8 +47,8 @@ CREATE TABLE medical_histories_treatments(
 id INT GENERATED ALWAYS AS IDENTITY,
 medical_histories_id INT,
 treatments_id INT,
-FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id),
-FOREIGN KEY (treatments_id) REFERENCES treatments(id),
+CONSTRAINT histories_id_fk FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id),
+CONSTRAINT treatments_id_fk FOREIGN KEY (treatments_id) REFERENCES treatments(id),
 PRIMARY KEY(id)
 ); 
 
