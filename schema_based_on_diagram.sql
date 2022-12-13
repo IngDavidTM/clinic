583c5fba91c26f1c273eab0a0a6e659a11d657fb
IngDavidTM <david-1011@hotmail.com>
2022-12-13 11:52:45 -0500

    add first three tables

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,25 @@
+CREATE TABLE patients(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(250),
+  date_of_birth DATE,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE medical_histories(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at timestamp,
+  patient_id INT,
+  FOREIGN KEY (patient_id) REFERENCES patients(id),
+  status VARCHAR(250),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE invoices(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount decimal,
+  generated_at timestamp,
+  payed_at timestamp,
+  medical_history_id INT,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+  PRIMARY KEY(id)
+);
