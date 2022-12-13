6795f8a9c1cfed2009da9de11c7117347a910b9b
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-12-13 16:24:03 -0300

    Add tables
    Add invoices, treatments, invoice_items and
    medical_histories_treatments tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -23,3 +23,31 @@ CREATE TABLE invoices(
   FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE treatments(
+id INT GENERATED ALWAYS AS IDENTITY,
+type VARCHAR(250),
+name VARCHAR(250),
+PRIMARY KEY(id)
+);
+
+CREATE TABLE invoice_items(
+id INT GENERATED ALWAYS AS IDENTITY,
+unit_price decimal,
+quantity INT,
+total_price decimal,
+invoice_id INT,
+treatment_id INT,
+FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+PRIMARY KEY(id)
+);
+
+CREATE TABLE medical_histories_treatments(
+id INT GENERATED ALWAYS AS IDENTITY,
+medical_histories_id INT,
+treatments_id INT,
+FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id),
+FOREIGN KEY (treatments_id) REFERENCES treatments(id),
+PRIMARY KEY(id)
+); 
